2574c8ebf85eb939d77a9f7f73fb629d4fa3f220
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-04-23 12:46:34 -0300

    Add Grunt in project

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      js: {
+        files: {
+          'src/js/main.min.js': ['bower_components/jquery/dist/jquery.min.js', 'src/js/main.js']
+        }
+      },
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'src/css/main.min.css': ['src/css/normalize.css', 'src/css/main.css']
+        }
+      }
+    },
+    shell: {
+      jekyllBuild: {
+        command: 'jekyll build'
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      css: {
+        files: ['src/css/**/*.css'],
+        tasks: ['cssmin', 'shell']
+      },
+      js: {
+        files: ['src/js/**/.js'],
+        tasks: ['uglify', 'shell']
+      },
+      html: {
+        files: ['**/*.html', '!./node_modules/*.html'],
+        tasks: ['shell'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 4000,
+          base: './_site'
+        }
+      }
+    },
+
+  });
+
+  grunt.registerTask('dev', ['cssmin', 'uglify', 'shell', 'connect', 'watch']);
+  grunt.registerTask('default', ['dev']);
+};
