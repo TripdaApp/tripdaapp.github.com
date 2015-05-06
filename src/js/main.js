283d3f7ca0ffbb9329cc69331faecc00ba206d13
Rafaell Lycan <sonny.webdsg@gmail.com>
2015-05-06 20:34:57 -0300

    Implements parseTemplate and parseCollection methods

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,49 @@
+var members = '<li>' +
+            '<img src="#image" alt="#name" class="photo">' +
+            '<a href="#link">@#name</a>' +
+            '</li>';
+
+function parseCollection (collection, template) {
+    if (!collection.length || !template) {
+        return false;
+    }
+
+    var length = collection.length;
+    var data = '';
+    for (var i = 0; i < length; i++) {
+        data += parseTemplate(collection[i], template);
+    };
+
+    return data;
+}
+
+function parseTemplate (data, template) {
+    console.log("Data", data);
+    var html;
+    if (template === 'members') {
+        html = String(members).replace('#image', data.avatar_url)
+            .replace('#name', data.login)
+            .replace('#link', data.html_url)
+            .replace('#name', data.login);
+    }
+
+    if (template === 'repos') {
+
+    }
+
+    return html;
+}
+
+
 jQuery(document).ready(function($) {
     $.get('https://api.github.com/orgs/tripdaapp/repos', function(data) {
         console.log("Repos", data);
     });
 
     $.get('https://api.github.com/orgs/tripdaapp/public_members', function(data) {
-        console.log("Members", data);
+        var members = parseCollection(data, 'members');
+        var list = $('#team-list');
+        list.append(members);
+        list.fadeIn(1000);
     });
 });
